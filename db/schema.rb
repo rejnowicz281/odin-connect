85e64ea4d59a7e0b9f5e5a9897209b422805fe76
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-01-07 12:43:34 +0100

    Create Posts controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_01_131042) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_07_111547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
